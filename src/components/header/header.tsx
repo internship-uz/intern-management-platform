6713cd0d1410e01ab950e0feccb7cb139d7d4d7a
alsheroziy <raxmatshehroz1@gmail.com>
2026-05-18 13:42:52 +0500

    fix header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,7 @@ import { HeaderSearch } from "./header-search";
 
 export function Header() {
   return (
-    <header className='flex h-14 shrink-0 items-center gap-3 border-b border-border/60 px-4'>
+    <header className='sticky top-0 z-30 flex h-14 shrink-0 items-center gap-3 border-b border-border/60 bg-background/95 px-4 backdrop-blur'>
       <SidebarTrigger className='-ml-1' />
       <HeaderSearch />
       <div className='ml-auto'>
